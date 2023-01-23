ac50fb70e00bc6f4f03b02debe6b79f9e85f9b69
Fabricio Porras Morera <faporras05@hotmail.com>
2023-01-23 12:14:33 -0600

    Add flutter animation and try to rotate

--- Airplane chicken game/Assets/Scripts/Chicken.cs
+++ Airplane chicken game/Assets/Scripts/Chicken.cs
@@ -22,15 +22,20 @@ public class Chicken : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    
     if(Input.GetKeyDown(KeyCode.W) && (gameManager.chicken.transform.position.y <= 3 && gameManager.chicken.transform.position.y >= -3))
         Jump();
-  
+    else
+    {
+        gameManager.chicken.transform.rotation = new Quaternion(0,0,gameManager.chicken.transform.rotation.z-0.0009f,1);
+    }
     
     }
     void Jump()
     {
-        gameManager.chicken.transform.position = new Vector3 (gameManager.chicken.transform.position.x,(gameManager.chicken.transform.position.y+0.9f),0);       
 
+        gameManager.chicken.transform.position = new Vector3 (gameManager.chicken.transform.position.x,(gameManager.chicken.transform.position.y+0.9f),0);       
+        gameManager.chicken.transform.rotation = new Quaternion(0,0,gameManager.chicken.transform.rotation.z+0.09f,1);
     }
 }
 
